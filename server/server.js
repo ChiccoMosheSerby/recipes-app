6ac07df013da44696c2a11e595f226eb693d6e43
Chicco Moshe Serby <chiccomoshe@gmail.com>
2020-06-02 23:15:18 +0300

    v10 with share

--- server/server.js
+++ server/server.js
@@ -17,46 +17,66 @@ const appKey = '27301a57cbfe5f373c242b8e58e39314';
 
 
 app.post('/getResipes', function (req, res) {
-    fetch(`https://api.edamam.com/search?q=${req.body.searchText}&app_id=${appId}&app_key=${appKey}`)
-        .then(data => {
-            data.json()
-                .then(finalData => {
-                    res.send(finalData)
+  fetch(`https://api.edamam.com/search?q=${req.body.searchText}&app_id=${appId}&app_key=${appKey}`)
+    .then(data => {
+      data.json()
+        .then(finalData => {
+          res.send(finalData)
 
-                })
         })
+    })
 });
 
 
 app.get('/shareResipe/:word/:id', function (req, res) {
-    fetch(`https://api.edamam.com/search?q=${req.params.word}&app_id=${appId}&app_key=${appKey}`)
-        .then(data => {
-            data.json()
-                .then(finalData => {
-                    let recipeArray = req.params.word;
-                    finalData.hits[req.params.id].recipe.ingredientLines.forEach(element => {
-                        recipeArray += `</br> ${element}`;
-                    });
+  fetch(`https://api.edamam.com/search?q=${req.params.word}&app_id=${appId}&app_key=${appKey}`)
+    .then(data => {
+      data.json()
+        .then(finalData => {
+          let recipeArray = req.params.word;
+          finalData.hits[req.params.id].recipe.ingredientLines.forEach(element => {
+            recipeArray += `</br> ${element}`;
+          });
 
-                    // console.dir(finalData.hits[req.body.id].recipe.label)
-                    // res.text(finalData.hits[req.params.id].recipe)
-                    res.send(`
-                    <div style="color:white; min-height:100vh; min-width:100vw; margin:0; padding:20px; background:#a30000; text-align:center;">
-                    <h1 >${finalData.hits[req.params.id].recipe.label}</h1>
-                    <img style="border-radius:15px; border:1px solid #fafafa65;" src='${finalData.hits[req.params.id].recipe.image}' />
-                    <div style="font-size:1.4rem; margin:10px; font-size:1.5rem; padding:10px;">
-
-                    ${recipeArray}
-                    
-                    </div >
-                    <a style="font-size:1.6rem; margin:100px; padding:20px; color:#ddcead;" href="https://recipes-app-prod.herokuapp.com">Go to:  Recipes App!</a>
-                    <img style=" margin:auto; border-radius:15px; border:1px solid #fafafa65;" src='./img/sticker-bon-appetit.png' />
+          // console.dir(finalData.hits[req.body.id].recipe.label)
+          // res.text(finalData.hits[req.params.id].recipe)
+          res.send(`
+                      <div style="word-break: break-word; color:white; margin:0; padding:20px; background:#a30000; text-align:center;">
+                                    <h1  style="font-size:55px;">${finalData.hits[req.params.id].recipe.label}</h1>
+                                    <img style="border-radius:15px; border:1px solid #fafafa65;" src='${finalData.hits[req.params.id].recipe.image}' />
+                                    <div style="word-break: break-word; font-size:38px; margin:10px; padding:10px;">
+                
+                                    ${recipeArray}
+                                    </br>
+                                    <a style="font-size:40px; margin:100px; padding:20px; color:#ddcead;" href="https://recipes-app-prod.herokuapp.com">Go to:  Recipes App! </br>
+                                    <img style="margin:auto; border-radius:15px; width:300px; height:auto; border:1px solid #fafafa65;" src='/img/sticker-bon-appetit.png' />
+                                    </a>
+                                    
+                                    </div>
                     </div>
                     `)
-                })
         })
+    })
 
 })
+
+
+// <div style="color:white; margin:0; padding:20px; background:#a30000; text-align:center;">
+// <h1  style="font-size:55px;">${finalData.hits[req.params.id].recipe.label}</h1>
+// <img style="border-radius:15px; border:1px solid #fafafa65;" src='${finalData.hits[req.params.id].recipe.image}' />
+// <div style="font-size:38px; margin:10px; padding:10px;">
+
+// ${recipeArray}
+
+// </div >
+// <a style="font-size:40px; margin:100px; padding:20px; color:#ddcead;" href="https://recipes-app-prod.herokuapp.com">Go to:  Recipes App!</a>
+// <img style="margin:auto; border-radius:15px; width:300px; height:auto; border:1px solid #fafafa65;" src='/img/sticker-bon-appetit.png' />
+// </div>
+
+
+
+
+
 
 //Or///////////////////////
 // fetch(`https://api.edamam.com/search?q=chicken&app_id=${appId}&app_key=${appKey}`)
@@ -75,7 +95,7 @@ app.get('/shareResipe/:word/:id', function (req, res) {
 //server conection------------------
 let port = process.env.PORT || 4005;
 app.listen(port, function () {
-    console.log('-------------------------> server conected to port: ', port, '!<---------------------------------------------------------')
+  console.log('-------------------------> server conected to port: ', port, '!<---------------------------------------------------------')
 })
 
 
